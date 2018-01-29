5c8da82e93c2274a3d3891fb972625e6598168e0
nterrill95 <nterrill@harding.edu>
2018-01-29 14:52:51 -0600

    Commit

---
+++ Boids/Assets/Boid.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+    private Neighborhood neighborhood;
+
+    void Awake()
+    {
+        neighborhood = GetComponent<Neighborhood>();
+        rigid = GetComponent<Rigidbody>();
+
+        pos = Random.insideUnitSphere * Spawner.S.spawnRadius;
+
+        Vector3 vel = Random.onUnitSphere * Spawner.S.velocity;
+        rigid.velocity = vel;
+
+        LookAhead();
+
+        Color randColor = Color.black;
+        while( randColor.r + randColor.g + randColor.b < 1.0f)
+        {
+            randColor = new Color(Random.value, Random.value, Random.value);
+        }
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach(Renderer r in rends)
+        {
+            r.material.color = randColor;
+        }
+        TrailRenderer tRend = GetComponent<TrailRenderer>();
+        tRend.material.SetColor("_TintColor", randColor);
+    }
+
+    void LookAhead()
+    {
+        transform.LookAt(pos + rigid.velocity);
+    }
+
+    public Vector3 pos
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 vel = rigid.velocity;
+        Spawner spn = Spawner.S;
+
+        Vector3 delta = Attractor.POS - pos;
+
+        Vector3 velAvoid = Vector3.zero;
+        Vector3 tooClosePos = neighborhood.avgClosePos;
+
+        if(tooClosePos != Vector3.zero)
+        {
+            velAvoid = pos - tooClosePos;
+            velAvoid.Normalize();
+            velAvoid *= spn.velocity;
+        }
+
+        Vector3 velAlign = neighborhood.avgVel;
+
+        if(velAlign != Vector3.zero)
+        {
+            velAlign.Normalize();
+            velAlign *= spn.velocity;
+        }
+
+        Vector3 velCenter = neighborhood.avgPos;
+
+        if(velCenter != Vector3.zero)
+        {
+            velCenter -= transform.position;
+            velCenter.Normalize();
+            velCenter *= spn.velocity;
+        }
+
+        bool attracted = (delta.magnitude > spn.attractPushDist);
+        Vector3 velAttract = delta.normalized * spn.velocity;
+
+        float fdt = Time.fixedDeltaTime;
+
+        if(attracted)
+        {
+            vel = Vector3.Lerp(vel, velAttract, spn.attractPull * fdt);
+        } 
+        else
+        {
+            vel = Vector3.Lerp(vel, -velAttract, spn.attractPush * fdt);
+        }
+
+        if(velAvoid != Vector3.zero)
+        {
+            vel = Vector3.Lerp(vel, velAvoid, spn.collAvoid * fdt);
+        } else
+        {
+            if(velAlign != Vector3.zero)
+            {
+                vel = Vector3.Lerp(vel, velAvoid, spn.velMatching * fdt);
+            }
+            if (velCenter != Vector3.zero)
+            {
+                vel = Vector3.Lerp(vel, velAvoid, spn.flockCentering * fdt);
+            }
+            if (velAttract != Vector3.zero)
+            {
+                if(attracted){
+                    vel = Vector3.Lerp(vel, velAvoid, spn.attractPull * fdt);
+                }
+                else
+                {
+                    vel = Vector3.Lerp(vel, -velAvoid, spn.attractPush * fdt);
+                }
+            }
+
+
+        }
+
+        vel = vel.normalized * spn.velocity;
+
+        rigid.velocity = vel;
+
+        LookAhead();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
